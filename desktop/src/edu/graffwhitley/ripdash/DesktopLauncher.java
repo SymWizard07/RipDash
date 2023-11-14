759f82093e829c48a14161ccaf2a26db042df657
SymWizard07 <90952234+SymWizard07@users.noreply.github.com>
2023-11-14 14:57:39 -0600

    initial commit

---
+++ desktop/src/edu/graffwhitley/ripdash/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package edu.graffwhitley.ripdash;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import edu.graffwhitley.ripdash.RdGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("RipDash");
+		new Lwjgl3Application(new RdGame(), config);
+	}
+}
