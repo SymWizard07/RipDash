1db898062537e252fef734d050b0e30e7549b61b
SymWizard07 <90952234+SymWizard07@users.noreply.github.com>
2023-11-16 17:21:33 -0600

    static tile update

--- core/src/edu/graffwhitley/ripdash/StaticTile.java
+++ core/src/edu/graffwhitley/ripdash/StaticTile.java
@@ -9,22 +9,29 @@ import com.badlogic.gdx.physics.box2d.World;
 
 public abstract class StaticTile {
     
+	static PolygonShape defaultShape;
+
+	static {
+		defaultShape = new PolygonShape();
+		defaultShape.setAsBox(1, 1);
+	}
+
+	protected BodyDef bodyDef;
     protected Body body;
     protected Sprite sprite;
 
-    StaticTile(World world, float x, float y) {
-        BodyDef groundBodyDef = new BodyDef();
-		groundBodyDef.position.set(new Vector2(0, -10)); // Set the position of the ground in the world
-		groundBodyDef.type = BodyDef.BodyType.StaticBody;
+    StaticTile(float x, float y) {
+        bodyDef = new BodyDef();
+		bodyDef.position.set(new Vector2(x, y)); // Set the position of the ground in the world
+		bodyDef.type = BodyDef.BodyType.StaticBody;
 
-		Body groundBody = world.createBody(groundBodyDef);
+		body = world.createBody(bodyDef);
 
-		PolygonShape groundBox = new PolygonShape();
-		groundBox.setAsBox(20 / 2, 1 / 2); // Set the size of the ground box
-
-		groundBody.createFixture(groundBox, 0.0f); // 0.0f is the density, which is irrelevant for static bodies
-
-		groundBox.dispose();
+		body.createFixture(StaticTile.tileShape, 0.0f);
     }
+
+	void createTile(World world) {
+		world.createBody(bodyDef);
+	}
 
 }
