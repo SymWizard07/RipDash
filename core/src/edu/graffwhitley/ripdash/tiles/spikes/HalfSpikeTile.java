f4b7c32da3ce5bbed2b2d872b844679b7babc63c
rgraff101 <rgraff@cub.uca.edu>
2023-12-09 18:35:04 -0600

    HalfSpike Class

---
+++ core/src/edu/graffwhitley/ripdash/tiles/spikes/HalfSpikeTile.java
@@ -0,0 +1,26 @@
+package edu.graffwhitley.ripdash.tiles.spikes;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+
+import edu.graffwhitley.ripdash.graphics.SpritePool;
+import edu.graffwhitley.ripdash.tiles.StaticTile;
+
+public class HalfSpikeTile extends StaticTile {
+
+    public static final int HALFSPIKE = SpritePool.addSprite("./Collision/HalfSpike.png", 23.0f, 12.5f);
+    public static final int FLIPPEDHALFSPIKE = SpritePool.addSprite("./Collision/FlippedHalfSpike.png", 23.0f, 12.5f);
+
+    public HalfSpikeTile(int poolIndex, float x, float y) {
+        super(poolIndex, x, y);
+        Vector2 vertex1 = new Vector2(-2 / 2, -2 / 2);
+        Vector2 vertex2 = new Vector2(2 / 2, -2 / 2);
+        Vector2 vertex3 = new Vector2(0, 2 / 2);
+
+        bodyShape = new PolygonShape();
+        bodyShape.set(new Vector2[] {vertex1, vertex2, vertex3});
+
+        
+    }
+    
+}
